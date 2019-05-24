694fac833bcb1cdeeebc3e2ce38c65446b77d827
Michael Blijleven <info@laravelontwikkelaar.com>
2019-05-24 15:52:07 +0200

    Route fixes

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::namespace('Voicecode\NavigationManager\Http\Controllers')->group(function() {
-    
-    Route::resource('navigation-manager', 'NavigationController');
+Route::namespace('Voicecode\NavigationManager\Http\Controllers')->group(function () {
+    Route::resource('navigation-manager', 'NavigationManagerController');
 
-    Route::prefix('navigation-manager')->group(function() {
-        Route::post('{id}/items/add', 'NavigationController@addItem')->where(['id' => '[0-9]+']);
-        Route::put('{id}/items/positions', 'NavigationController@updatePositions')->where(['id' => '[0-9]+']);
-        Route::put('{id}/items/{itemId}', 'NavigationController@updateItem')->where(['id' => '[0-9]+'])->where(['itemId' => '[0-9]+']);
-        Route::delete('{id}/items/{itemId}', 'NavigationController@deleteItem')->where(['id' => '[0-9]+'])->where(['itemId' => '[0-9]+']);
+    Route::prefix('navigation-manager')->group(function () {
+        Route::post('{id}/items/add', 'NavigationManagerController@addItem')->where(['id' => '[0-9]+']);
+        Route::put('{id}/items/positions', 'NavigationManagerController@updatePositions')->where(['id' => '[0-9]+']);
+        Route::put('{id}/items/{itemId}', 'NavigationManagerController@updateItem')->where(['id' => '[0-9]+'])->where(['itemId' => '[0-9]+']);
+        Route::delete('{id}/items/{itemId}', 'NavigationManagerController@deleteItem')->where(['id' => '[0-9]+'])->where(['itemId' => '[0-9]+']);
     });
 });
